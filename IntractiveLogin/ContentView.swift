e63c5b7afe2c9b0ba7dc94e00e562c5ca38562a9
yashChouhanGit <87747899+yashChouhanGit@users.noreply.github.com>
2026-03-29 18:05:05 +0530

    IntractiveLogin Init

---
+++ IntractiveLogin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IntractiveLogin
+//
+//  Created by Yash Chouhan on 29/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IntractiveLogin/IntractiveLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntractiveLoginApp.swift
+//  IntractiveLogin
+//
+//  Created by Yash Chouhan on 29/03/26.
+//
+
+import SwiftUI
+
+@main
+struct IntractiveLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
